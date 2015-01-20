e39d1dd3c4fba1be2eea541f9b36f2bf5e6fc159
Connor McEwen <connor.mcewen@gmail.com>
2015-01-20 18:15:20 -0500

    Add keywords and refactor into a Card class

--- lib/wildcard-pair/ArticleCard.rb
+++ lib/wildcard-pair/ArticleCard.rb
@@ -1,44 +1,25 @@
 #!/usr/bin/env ruby -wKU
 
-require 'active_model'
-require_relative 'hash_mappable.rb'
+require_relative 'Card.rb'
 require_relative 'Media.rb'
 
 module WildcardPair
-  class ArticleCard
+  class ArticleCard < WildcardPair::Card
     private
 
-    attr_accessor :article, :card_type, :pair_version
+    attr_accessor :article
 
     public
 
-    include ActiveModel::Validations
-    include ActiveModel::Serializers::JSON
-    include WildcardPair::HashMappable
     include WildcardPair::Media
 
-    attr_accessor :web_url
-    attr_reader :article, :card_type, :pair_version
+    attr_reader :article
 
-    validates :web_url, presence: true
     validate :validate_article
 
     def initialize(attributes = {})
-      attributes.each do |name, value|
-        send("#{name}=", value)
-      end
-
+      super
       @card_type = 'article'
-
-      if !Gem.loaded_specs['wildcard-pair'].nil?
-        @pair_version = Gem.loaded_specs['wildcard-pair'].version.to_s
-      else
-        @pair_version = "unknown"
-      end
-    end
-
-    def attributes
-      instance_values
     end
 
     def populate_from_metatags(web_url)
@@ -65,20 +46,6 @@ module WildcardPair
         end
       end
     end
-
-    #exclude validation fields in the JSON output
-    def as_json(options={})
-      super(options.merge({:except => [:errors, :validation_context]}))
-    end
-
-    def to_json(options={})
-      if self.valid?
-        super(options)
-      else
-        raise "Article Card is not valid - please remedy the following errors:" << self.errors.messages.to_s
-      end    
-    end 
-
   end
 end
 
