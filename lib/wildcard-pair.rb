1aacfb77a745abd6cbabdbc5f7cf9eb501e04f9a
Andrew Lum <Andrew@trywildcard.com>
2015-04-13 19:07:34 -0400

    adding requires to the wildcard-pair gem to force load order

--- lib/wildcard-pair.rb
+++ lib/wildcard-pair.rb
@@ -1,3 +1,9 @@
+require 'wildcard-pair/Card.rb'
+require 'wildcard-pair/Media.rb'
+require 'wildcard-pair/Media/Image.rb'
+require 'wildcard-pair/Media/Video.rb'
+require 'wildcard-pair/Object.rb'
+
 Gem.find_files("wildcard-pair/*.rb").each { |path| require path }
 Gem.find_files("wildcard-pair/*/*.rb").each { |path| require path }
 
